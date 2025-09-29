341f37e54eccf012afec02157214a97b94f027f2
Makarios <makarioskulik@gmail.com>
2025-09-30 00:57:56 +0300

    Update main.cpp
    Обновление файла main.cpp в ветке extended

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     double a, b;
     char op;
 
-    cout << "Калькулятор v1 (базовый)\n";
-    cout << "Введите выражение (например: 2 + 3): ";
+    cout << "Калькулятор v2 (расширенный)\n";
+    cout << "Операции: + - * / ^ s (sqrt)\n";
+    cout << "Введите выражение (например: 2 ^ 3 или 9 s 0): ";
 
     cin >> a >> op >> b;
 
@@ -16,9 +18,13 @@ int main() {
     else if (op == '/') {
         if (b == 0) cout << "Ошибка: деление на ноль!" << endl;
         else cout << "Результат: " << a / b << endl;
-    } else {
-        cout << "Неизвестная операция!" << endl;
     }
+    else if (op == '^') cout << "Результат: " << pow(a, b) << endl;
+    else if (op == 's') { // sqrt(a), b игнорируем
+        if (a < 0) cout << "Ошибка: отрицательное число!" << endl;
+        else cout << "Результат: " << sqrt(a) << endl;
+    }
+    else cout << "Неизвестная операция!" << endl;
 
     return 0;
 }
